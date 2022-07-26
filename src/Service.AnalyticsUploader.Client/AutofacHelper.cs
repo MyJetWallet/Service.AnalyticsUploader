1fb687fd8da770927de0227ebfd11095e8db13e7
Alexey Novichikhin <alexey.n@smplt.net>
2022-07-26 22:53:44 +0300

    fix issue with AutofacHelper

--- src/Service.AnalyticsUploader.Client/AutofacHelper.cs
+++ src/Service.AnalyticsUploader.Client/AutofacHelper.cs
@@ -5,7 +5,7 @@ namespace Service.AnalyticsUploader.Client;
 
 public static class AutofacHelper
 {
-    public static void RegisterKycStatusClients(this ContainerBuilder builder, string myNoSqlWriterUrl)
+    public static void RegisterAnalyticsUploaderClients(this ContainerBuilder builder, string myNoSqlWriterUrl)
     {
         builder.RegisterMyNoSqlWriter<AnalyticIdToClientNoSql>(() => myNoSqlWriterUrl, AnalyticIdToClientNoSql.TableName);
 
